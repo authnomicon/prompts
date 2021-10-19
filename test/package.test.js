5b62fe52b02205348c4a4eb5901120797d0225ec
Jared Hanson <jaredhanson@gmail.com>
2021-10-18 17:02:47 -0700

    Add makefile and tests.

---
+++ test/package.test.js
@@ -0,0 +1,27 @@
+/* global describe, it */
+
+var expect = require('chai').expect;
+
+
+describe('@authnomicon/prompts', function() {
+  
+  describe('package.json', function() {
+    var json = require('../package.json');
+    
+    it('should have assembly metadata', function() {
+      expect(json.assembly.namespace).to.equal('org.authnomicon/prompts');
+      expect(json.assembly.components).to.deep.equal([
+        'http/registry',
+        'http/login',
+        'http/select-account'
+      ]);
+    });
+  });
+  
+  it('should throw if required', function() {
+    expect(function() {
+      var pkg = require('..');
+    }).to.throw(Error).with.property('code', 'MODULE_NOT_FOUND');
+  });
+  
+});
