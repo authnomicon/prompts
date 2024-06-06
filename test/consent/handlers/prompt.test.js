73e589014fe9f9007aa4d276be704e84ba2fea5b
Jared Hanson <jaredhanson@gmail.com>
2024-06-06 10:14:08 -0700

    Add tests.

---
+++ test/consent/handlers/prompt.test.js
@@ -0,0 +1,35 @@
+/* global describe, it, expect */
+
+var expect = require('chai').expect;
+var chai = require('chai');
+var $require = require('proxyquire');
+var sinon = require('sinon');
+var factory = require('../../../com/consent/handlers/prompt');
+
+
+describe('consent/handlers/prompt', function() {
+  
+  it('should create handler', function() {
+    var csurfSpy = sinon.spy();
+    var flowstateSpy = sinon.spy();
+    var factory = $require('../../../com/consent/handlers/prompt', {
+      'csurf': csurfSpy,
+      'flowstate': flowstateSpy
+    });
+    
+    var authenticator = new Object();
+    authenticator.authenticate = sinon.spy();
+    var store = new Object();
+    var handler = factory(authenticator, store);
+    
+    expect(handler).to.be.an('array');
+    expect(csurfSpy).to.be.calledOnce;
+    expect(csurfSpy).to.be.calledBefore(flowstateSpy);
+    expect(flowstateSpy).to.be.calledOnce;
+    expect(flowstateSpy).to.be.calledWith({ store: store });
+    expect(flowstateSpy).to.be.calledBefore(authenticator.authenticate);
+    expect(authenticator.authenticate).to.be.calledOnce;
+    expect(authenticator.authenticate).to.be.calledWith('session');
+  });
+  
+});
