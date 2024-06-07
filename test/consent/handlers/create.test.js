6f24d938dc56eef829061a70d743663e9c1c533b
Jared Hanson <jaredhanson@gmail.com>
2024-06-07 15:28:03 -0700

    Add tests.

--- test/consent/handlers/create.test.js
+++ test/consent/handlers/create.test.js
@@ -1,1 +1,40 @@
-// TODO
+/* global describe, it, expect */
+
+var expect = require('chai').expect;
+var chai = require('chai');
+var $require = require('proxyquire');
+var sinon = require('sinon');
+var factory = require('../../../com/consent/handlers/create');
+
+
+describe('consent/handlers/create', function() {
+  
+  it('should create handler', function() {
+    var bodyParserSpy = sinon.spy();
+    var csurfSpy = sinon.spy();
+    var flowstateSpy = sinon.spy();
+    var factory = $require('../../../com/consent/handlers/create', {
+      'body-parser': { urlencoded: bodyParserSpy },
+      'csurf': csurfSpy,
+      'flowstate': flowstateSpy
+    });
+    
+    var authenticator = new Object();
+    authenticator.authenticate = sinon.spy();
+    var store = new Object();
+    var handler = factory(undefined, authenticator, store);
+    
+    expect(handler).to.be.an('array');
+    expect(bodyParserSpy).to.be.calledOnce;
+    expect(bodyParserSpy).to.be.calledWith({ extended: false });
+    expect(bodyParserSpy).to.be.calledBefore(csurfSpy);
+    expect(csurfSpy).to.be.calledOnce;
+    expect(csurfSpy).to.be.calledBefore(flowstateSpy);
+    expect(flowstateSpy).to.be.calledOnce;
+    expect(flowstateSpy).to.be.calledWith({ store: store });
+    expect(flowstateSpy).to.be.calledBefore(authenticator.authenticate);
+    expect(authenticator.authenticate).to.be.calledOnce;
+    expect(authenticator.authenticate).to.be.calledWith('session');
+  });
+  
+});
