879eedf7d1facd85f34aa061850cba1a24c982b8
Jared Hanson <jaredhanson@gmail.com>
2024-06-06 20:50:55 -0700

    Add client and scope options to consent prompt.

--- com/consent/prompt.js
+++ com/consent/prompt.js
@@ -1,7 +1,13 @@
+var url = require('url');
+
 exports = module.exports = function() {
   
   return function(req, res, next) {
-    return res.redirect('/consent');
+    var q = {};
+    if (res.locals.client) { q.client_id = res.locals.client.id; }
+    if (res.locals.scope) { q.scope = res.locals.scope.join(' '); }
+    
+    return res.redirect(url.format({ pathname: '/consent', query: q }));
   };
 };
 
